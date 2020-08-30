e8d84d6514da178c7c42b2cc3981568e3f266354
Clark Allen <kitico@gmail.com>
2020-08-30 19:37:01 -0400

    more problems... more tests.

--- src/main/java/net/kbg/probs/mcdowell/Ch1.java
+++ src/main/java/net/kbg/probs/mcdowell/Ch1.java
@@ -1,8 +1,6 @@
 package net.kbg.probs.mcdowell;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Ch1 {
 
@@ -68,6 +66,164 @@ public class Ch1 {
         } else {
             tally.put(key, 1 + tally.get(key));
         }
+    }
+
+    /*
+        Replace all the space characters in a string with '%20'.
+     */
+    public String URLify(String url) {
+        return url.replaceAll("\\ ", "%20");
+    }
+
+    /*
+        Determine if a string is the permutation of a palindrome.
+        Ignore case and non-letter characters.
+     */
+    public boolean isPalindromePermutation(String arg) {
+        if (arg == null) {
+            throw new IllegalArgumentException();
+        }
+        String trim = arg.replaceAll("[-., :;!?()#%@$&*/]", "");
+        trim = trim.toLowerCase();
+
+        if (trim.length() < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        if (trim.length() == 1) {
+            return true;
+        }
+
+        Map<String, Integer> charCount = new HashMap<>();
+        mapChars(trim, charCount);
+
+        int oddCount = 0;
+
+        if (trim.length() % 2 == 1) {
+            // the string has an odd length
+            for (String key : charCount.keySet()) {
+                if (charCount.get(key) % 2 == 1) {
+                    ++oddCount;
+                }
+            }
+            if (oddCount == 1) {
+                return true;
+            }
+            return false;
+        }
+
+        // The string has an even length
+        for (String key : charCount.keySet()) {
+            if (charCount.get(key) % 2 == 1) {
+                ++oddCount;
+            }
+        }
+
+        if (oddCount % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+        Given two strings, determine if they are one edit or zero
+        edits from being the same where these edits are allowed
+        (that is, if they are more than one edit different or
+        less than two edits different) :
+            1. insert a character
+            2. remove a character
+            3. replace a character
+     */
+    public boolean isOneAway(String arg1, String arg2) {
+        if (arg1 == null || arg1.isEmpty() || arg2 == null || arg2.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Character> ls1 = convertToList(arg1.toCharArray());
+        List<Character> ls2 = convertToList(arg2.toCharArray());
+        int diffs = Math.abs(ls1.size() - ls2.size());
+
+        if (diffs > 1) {
+            return false;
+        }
+        if (diffs == 1) {
+            // then check insert or remove
+            if (ls1.size() > ls2.size()) {
+                for (int g = 0; g < ls2.size(); ++g) {
+                    if ( ! ls1.get(g).equals(ls2.get(g)) &&
+                        ! ls1.get(g + 1).equals(ls2.get(g))) {
+                        ++diffs;
+                        break;
+                    }
+                }
+            }
+        } else {
+            // diffs == 0
+            // then check for replace
+            for (int g = 0; g < ls1.size(); ++g) {
+                if (ls1.get(g) != ls2.get(g)) {
+                    ++diffs;
+                }
+                if (diffs > 1) {
+                    break;
+                }
+            }
+        }
+        return diffs < 2;
+    }
+
+    private List<Character> convertToList(char[] chars) {
+        List<Character> clist = new ArrayList<>(100);
+        for (char c : chars) {
+            clist.add(c);
+        }
+        return clist;
+    }
+
+    public String compressString(String arg) {
+        if (arg == null || arg.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (arg.length() == 1) {
+            return arg;
+        }
+
+        List<Character> chars = convertToList(arg.toCharArray());
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = 0;
+        String prev = null;
+
+        for (int k = 0; k < chars.size(); ++k) {
+            String c = chars.get(k).toString();
+            if (k == 0) {
+                ++cnt;
+                prev = c;
+            } else if (k == chars.size() - 1) {
+                if (c.equals(prev)) {
+                    ++cnt;
+                    sb.append(c).append(cnt);
+                } else {
+                    sb.append(prev);
+                    if (cnt > 1) {
+                        sb.append(cnt);
+                    }
+                    sb.append(c);
+                }
+            }
+            else if (c.equals(prev)) {
+                ++cnt;
+            } else {
+                sb.append(prev);
+                if (cnt > 1) {
+                    sb.append(cnt);
+                }
+                cnt = 1;
+                prev = c;
+            }
+
+        }
+        return sb.toString();
     }
 
 }
--- src/test/java/net/kbg/probs/mcdowell/Ch1Test.java
+++ src/test/java/net/kbg/probs/mcdowell/Ch1Test.java
@@ -1,15 +1,21 @@
 package net.kbg.probs.mcdowell;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class Ch1Test {
 
+    private Ch1 ch1;
+
+    @Before
+    public void init() {
+        ch1 = new Ch1();
+    }
+
     @Test
     public void testIsUnique() {
-        Ch1 ch1 = new Ch1();
         assertFalse(ch1.isUnique("ghbadeafci"));
         assertFalse(ch1.isUnique("gg"));
 
@@ -26,9 +32,81 @@ public class Ch1Test {
         String s3 = "madamimadaa";  // slightly different, same length
         String s4 = "blivit";  // different length.
 
-        Ch1 ch1 = new Ch1();
         assertTrue(ch1.isPermutation(s1, s2));
         assertFalse(ch1.isPermutation(s1, s3));
         assertFalse(ch1.isPermutation(s1, s4));
+    }
+
+    @Test
+    public void testURLify() {
+        String before = "http://something/dirty string for mommy";
+        String expected = "http://something/dirty%20string%20for%20mommy";
+        String actual = ch1.URLify(before);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsPalindromePermutation() {
+        String input = "Tact Coa"; // Odd length after cleaning
+        assertTrue(ch1.isPalindromePermutation(input));
+
+        input = "Tact Coa O";  // Even length after cleaning
+        assertTrue(ch1.isPalindromePermutation(input));
+
+        input = " G  ";  // Length = 1 after cleaning
+        assertTrue(ch1.isPalindromePermutation(input));
+    }
+
+    @Test
+    public void testIsOneAway() {
+        String inp1 = "pale";
+        String inp2 = "ple";
+        assertTrue(ch1.isOneAway(inp1, inp2));
+
+        inp1 = "pales";
+        inp2 = "pale";
+        assertTrue(ch1.isOneAway(inp1, inp2));
+
+        inp1 = "pale";
+        inp2 = "bale";
+        assertTrue(ch1.isOneAway(inp1, inp2));
+
+        inp1 = "pale";
+        inp2 = "bake";
+        assertFalse(ch1.isOneAway(inp1, inp2));
+    }
+
+    @Test
+    public void testCompressString() {
+        String input = "aabcccccaaa";
+        String expected = "a2bc5a3";
+        String actual = ch1.compressString(input);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
+        input = "c";
+        expected = "c";
+        actual = ch1.compressString(input);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
+        input = "cc";
+        expected = "c2";
+        actual = ch1.compressString(input);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
+        input = "cbc";
+        expected = "cbc";
+        actual = ch1.compressString(input);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
+        input = "abcde";
+        expected = "abcde";
+        actual = ch1.compressString(input);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
     }
 }
