f4a56fb67461e31c1b3d98ab199f747ddce84f66
Clark Allen <kitico@gmail.com>
2020-09-02 17:43:28 -0400

    A generic bubble sort

---
+++ src/main/java/net/kbg/algo/sort/GenBubbleSort.java
@@ -0,0 +1,25 @@
+package net.kbg.algo.sort;
+
+import java.util.List;
+
+public class GenBubbleSort<T extends Comparable<T>> {
+
+    public void sort(List<T> items) {
+        if (items == null) {
+            throw new IllegalArgumentException();
+        }
+        for (int k = 0; k < items.size(); ++k) {
+            for (int b = 0; b < items.size() - 1; ++b) {
+                if (items.get(b).compareTo(items.get(b+1)) > 0) {
+                    swap(b, b+1, items);
+                }
+            }
+        }
+    }
+
+    private void swap(int idx1, int idx2, List<T> items) {
+        T buf = items.get(idx1);
+        items.set(idx1, items.get(idx2));
+        items.set(idx2, buf);
+    }
+}
