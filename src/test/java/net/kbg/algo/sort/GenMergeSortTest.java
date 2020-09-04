354ee9e8e82ff08d526487f681cf2e5856c7f74e
Clark Allen <kitico@gmail.com>
2020-09-04 19:46:05 -0400

    added a test of a different object type.

--- src/test/java/net/kbg/algo/sort/GenMergeSortTest.java
+++ src/test/java/net/kbg/algo/sort/GenMergeSortTest.java
@@ -2,6 +2,7 @@ package net.kbg.algo.sort;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,5 +24,29 @@ public class GenMergeSortTest {
         for (int k = 0, b = 1; b < list.size(); ++k, ++b) {
             assertTrue(list.get(k).compareTo(list.get(b)) <= 0);
         }
+    }
+
+    @Test
+    public void testSortIntegers() {
+        GenMergeSort<Integer> mergeSort = new GenMergeSort<>();
+        List<Integer> list = makeIntegerList();
+        int len = list.size();
+        System.out.println(list);
+        mergeSort.sort(list);
+        System.out.println(list);
+        assertEquals(len, list.size());
+        for (int k = 0, b = 1; b < list.size(); ++k, ++b) {
+            assertTrue(list.get(k).compareTo(list.get(b)) <= 0);
+        }
+    }
+
+    private List<Integer> makeIntegerList() {
+        List<Integer> list = new ArrayList<>();
+        list.add(19);
+        list.add(6);
+        list.add(11);
+        list.add(22);
+        list.add(4);
+        return list;
     }
 }
